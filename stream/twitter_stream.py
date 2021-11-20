326ed8c9634e914b73d02922813d515045d82064
PaulinaPacyna <pacyna1998@gmail.com>
2021-11-20 13:43:10 +0100

    black + Unix timestamp

--- stream/twitter_stream.py
+++ stream/twitter_stream.py
@@ -5,62 +5,64 @@ import boto3
 import pandas as pd
 import tweepy
 
+ssm = boto3.client("ssm", region_name="us-east-1")
+
 
 class StreamListener(tweepy.Stream):
-
-    def __init__(self, nifi_instance_public_ip, save_path='saved_tweets', **kwargs):
+    def __init__(self, nifi_instance_public_ip, save_path="saved_tweets", **kwargs):
         super().__init__(**kwargs)
         self.save_path = save_path
-        self.fileds = ['text', 'id', 'created_at', 'user', 'geo', 'lang']
+        self.fileds = ["text", "id", "created_at", "user", "geo", "lang"]
         self.nifi_instance_public_ip = nifi_instance_public_ip
 
-    def send_data(self, data, nifi_instance_public_ip):
-        r = requests.post(f"http://{nifi_instance_public_ip}:7001/twitterListener",
-                          json=data)
+    def send_data(self, data):
+        r = requests.post(
+            f"http://{self.nifi_instance_public_ip}:7001/twitterListener", json=data
+        )
         print(r.status_code)
 
     def on_status(self, status):
         print(status.text)
         data = {
-            'text': status.text,
-            'id': status.id,
-            'created_at': str(status.created_at),
-            'geo': status.geo,
-            'lang': status.lang,
-            'user': status.user.screen_name
+            "text": status.text,
+            "id": status.id,
+            "created_at": int(status.created_at.timestamp()),
+            "geo": status.geo,
+            "lang": status.lang,
+            "user": status.user.screen_name,
         }
-        self.send_data(data=data, nifi_instance_public_ip=self.nifi_instance_public_ip)  # look at TODO
-        with open(f'{self.save_path}/{status.id}.json', 'w') as f:
+        self.send_data(data=data)  # look at TODO
+        with open(f"{self.save_path}/{status.id}.json", "w") as f:
             json.dump(data, f)
 
 
 def get_credentials():
-    ssm = boto3.client("ssm", region_name='us-east-1')
     return {
-        'consumer_key': ssm.get_parameter(Name="consumer_key")["Parameter"]["Value"],
-        'consumer_secret': ssm.get_parameter(Name="consumer_secret")["Parameter"]["Value"],
-        'access_token': ssm.get_parameter(Name="access_token")["Parameter"]["Value"],
-        'access_token_secret': ssm.get_parameter(Name="access_token_secret")["Parameter"]["Value"],
+        "consumer_key": ssm.get_parameter(Name="consumer_key")["Parameter"]["Value"],
+        "consumer_secret": ssm.get_parameter(Name="consumer_secret")["Parameter"][
+            "Value"
+        ],
+        "access_token": ssm.get_parameter(Name="access_token")["Parameter"]["Value"],
+        "access_token_secret": ssm.get_parameter(Name="access_token_secret")[
+            "Parameter"
+        ]["Value"],
     }
 
+
 def get_nifi_public_ip():
-    ssm = boto3.client("ssm", region_name='us-east-1')
     return ssm.get_parameter(Name="NIFI_PUBLIC_IP")["Parameter"]["Value"]
 
 
 def main():
-    os.makedirs('saved_tweets', exist_ok=True)
+    os.makedirs("saved_tweets", exist_ok=True)
     credentials = get_credentials()
     nifi_public_ip = get_nifi_public_ip()
-    accounts = pd.read_csv('accounts.csv')
-    stream = StreamListener(
-    	nifi_public_ip,
-        **credentials
-    )
+    accounts = pd.read_csv("accounts.csv")
+    stream = StreamListener(nifi_instance_public_ip=nifi_public_ip, **credentials)
     stream.filter(
         follow=accounts.id.tolist(),
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
