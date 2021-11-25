ef08564173b12f2cf1c3285d41317cf22d4f4f4c
PaulinaPacyna <pacyna1998@gmail.com>
2021-11-25 20:22:07 +0100

    refactor

--- lambdas/reddit_edit_data_lambda/reddit-lambda.py
+++ lambdas/reddit_edit_data_lambda/reddit-lambda.py
@@ -1,13 +1,12 @@
-##python lib for aws
 import base64
 import json
 
-##python lib for aws
 import boto3
-
+from boto3.dynamodb.types import TypeSerializer
 ssm = boto3.client("ssm", region_name="us-east-1")
 
 dynamodb = boto3.client('dynamodb', region_name="us-east-1")
+serializer = TypeSerializer()
 
 
 def get_nifi_public_ip():
@@ -16,21 +15,10 @@ def get_nifi_public_ip():
 
 def lambda_handler(event, context):
     print(event)
-    try:
-        for record in event["Records"]:
-            decoded_data = base64.b64decode(record["kinesis"]["data"]).decode("utf-8")
+    for record in event["Records"]:
+        decoded_data = base64.b64decode(record["kinesis"]["data"]).decode("utf-8")
 
-            print(decoded_data)
-            data = json.loads(decoded_data)
-            dynamo_types = {
-                str(int): 'N',
-                str(float): 'N',
-                str(str): 'S',
-                str(bool): 'BOOL',
-                str(dict): 'M',
-            }
-            for k in data:
-                data[k] = {dynamo_types[str(type(data[k]))]: str(data[k])}
-            dynamodb.put_item(TableName='RedditPosts', Item=data)
-    except Exception as e:
-        print(str(e))
+        print(decoded_data)
+        data = json.loads(decoded_data)
+        data = {key: serializer.serialize(value) for key, value in data.items()}
+        dynamodb.put_item(TableName='RedditPosts', Item=data)
