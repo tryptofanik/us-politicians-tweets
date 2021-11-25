345c8af0cc4737f3894d73e2e43dd94bcb1c23fa
PaulinaPacyna <pacyna1998@gmail.com>
2021-11-25 20:16:58 +0100

    added kinesis between nifi and Dynamo

---
+++ lambdas/twitter-kinesis-lambda/twitter-kinesis-lambda.py
@@ -0,0 +1,24 @@
+import base64
+import json
+
+import boto3
+from boto3.dynamodb.types import TypeSerializer
+ssm = boto3.client("ssm", region_name="us-east-1")
+
+dynamodb = boto3.client('dynamodb', region_name="us-east-1")
+serializer = TypeSerializer()
+
+
+def get_nifi_public_ip():
+    return ssm.get_parameter(Name="NIFI_PUBLIC_IP")["Parameter"]["Value"]
+
+
+def lambda_handler(event, context):
+    print(event)
+    for record in event["Records"]:
+        decoded_data = base64.b64decode(record["kinesis"]["data"]).decode("utf-8")
+
+        print(decoded_data)
+        data = json.loads(decoded_data)
+        data = {key: serializer.serialize(value) for key, value in data.items()}
+        dynamodb.put_item(TableName='Tweets', Item=data)
