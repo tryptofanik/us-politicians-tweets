da47dce2cbad5afc17e05083a8f1e0783ea42600
PaulinaPacyna <pacyna1998@gmail.com>
2021-12-29 13:15:20 +0100

    password in paramstore

--- spark/set_jupyter_passwd.py
+++ spark/set_jupyter_passwd.py
@@ -1,4 +1,5 @@
 # this script sets a password to the jupyter notebook served on ec2
 from IPython.lib import passwd
+import boto3
 with open("/root/.jupyter/jupyter_notebook_config.py", "a") as f:
-    f.write("\nconf.NotebookApp.password =u\"" + passwd("lotlr") + "\"\n")
+    f.write("\nconf.NotebookApp.password =u\"" + passwd(boto3.client("ssm", region_name="us-east-1").get_parameter(Name="jupyter-password")["Parameter"]["Value"]) + "\"\n")
