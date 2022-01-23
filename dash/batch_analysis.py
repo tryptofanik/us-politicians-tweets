ab7e576cc3394e34f63ceae5902a58fdfcff454c
kbogacka <kbogacka766@gmail.com>
2022-01-23 16:21:33 +0100

    Add dash visualization

--- dash/batch_analysis.py
+++ dash/batch_analysis.py
@@ -1,28 +1,22 @@
 import os
-import time 
-import datetime
 
 import boto3
-from boto3.dynamodb.conditions import Key, Attr
 import dash
-import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Input, Output
+import dash_html_components as html
 import dash_table
 import numpy as np
-import plotly.graph_objects as go
 import pandas as pd
 import plotly.express as px
-
+from dash.dependencies import Input, Output
 
 dynamodb = boto3.resource('dynamodb',
                           aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
                           aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
                           region_name='us-east-1'
-)
+                          )
 
 app = dash.Dash()
-
 
 tweets = dynamodb.Table('Tweets').scan(Limit=10000)['Items']
 tweets = pd.DataFrame(tweets)
@@ -30,14 +24,14 @@ tweets['ts'] = tweets.extractionTime.astype(int)
 tweets['date'] = pd.to_datetime(tweets['ts'], unit='s')
 tweets = tweets.loc[tweets.date > '2022-01-20']
 marks = {
-    k: k #str(pd.to_datetime(k, unit='s'))
+    k: k  # str(pd.to_datetime(k, unit='s'))
     for k in np.linspace(tweets.ts.min(), tweets.ts.max(), 4).astype(int)
 }
 
 agged_tweets = (
     tweets.assign(count=1).groupby('user').count()
     ['count'].reset_index()
-    .sort_values('count', ascending=False)
+        .sort_values('count', ascending=False)
 )
 
 app.layout = html.Div([
@@ -56,7 +50,7 @@ app.layout = html.Div([
         id='table',
         columns=[{"name": i, "id": i} for i in agged_tweets.columns],
         data=agged_tweets.to_dict('records')
-    )    
+    )
 ])
 
 
@@ -82,10 +76,10 @@ def update_table(value):
     agged_tweets_ = (
         data.assign(count=1).groupby('user').count()
         ['count'].reset_index()
-        .sort_values('count', ascending=False)
+            .sort_values('count', ascending=False)
     )
     return agged_tweets_.to_dict('records')
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, )
+    app.run_server(debug=True, port=8051)
